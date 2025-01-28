b3c8119648a4a05dfcf858c2a9a9605016ee4027
ryannolanco <ryannolanco@gmail.com>
2025-01-28 04:13:45 -0700

    complete: filterRecipes function

--- src/components/AllergyFilter.jsx
+++ src/components/AllergyFilter.jsx
@@ -1,20 +1,35 @@
 import React from 'react';
 import ingredients from '../db/data.jsx';
-import dishes from '../db/dishes.jsx';
+import recipes from '../db/dishes.jsx';
+
+const allergies = ['corn', 'basil', 'honey', 'lettuce', 'milk '];
+
+function filterRecipes(allergies, recipes) {
+	const normalizedAllergies = allergies.map((allergy) => allergy.toLowerCase());
+
+	return recipes.filter((recipe) => {
+		const normalizedIngredients = recipe.ingredients.map((ingredient) =>
+			ingredient.toLowerCase()
+		);
+
+		return !normalizedAllergies.some((allergy) =>
+			normalizedIngredients.includes(allergy)
+		);
+	});
+}
+
+const safeDishes = filterRecipes(allergies, recipes);
 
 const AllergyFilter = () => {
 	return (
 		<div>
 			{' '}
 			<div>
+				<p>{`All recipes amount ${recipes.length}`}</p>
+				<p>{`Filtered recipes amount ${safeDishes.length}`}</p>
 				<ul>
-					{ingredients.map((ingredient) => (
-						<li>{ingredient}</li>
-					))}
-				</ul>
-				<ul>
-					{dishes.map((dish) => (
-						<li>{dish}</li>
+					{safeDishes.map((dish) => (
+						<li>{dish.name}</li>
 					))}
 				</ul>
 			</div>
